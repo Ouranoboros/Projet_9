f87ad6025b33910e5a456c92438fa982d2a565c3
Ouranoboros <pierre.moutiers@gmail.com>
2023-02-14 18:54:08 +0100

    Cours mardi

--- Projet/Soutenance.py
+++ Projet/Soutenance.py
@@ -1,32 +1,34 @@
 #Importation des différentes librairies nécessaires 
 import requests
 from bs4 import BeautifulSoup
-#import pandas as pd
+import pandas as pd
 
 
 #Url de base de la centrale
 url = """https://www.lacentrale.fr/listing?energies={energy}&makesModelsCommercialNames={brand}&mileageMax={km_max}&mileageMin={km_min}&priceMax={price_max}&priceMin={price_min}yearMax={year_max}&yearMin={year_min}"""
+
 #spécification des différents paramètres
-parameters = url.format(energy="ess",brand="PEUGEOT",km_max=250000,km_min=10000,price_max=60000,price_min=10000,year_max=2022,year_min=2019)
-print (parameters)
+url = url.format(energy="ess",brand="PEUGEOT",km_max=250000,km_min=10000,price_max=60000,price_min=10000,year_max=2022,year_min=2019)
+print (url)
 
 #Requête de l'url
-request= requests.get(url)
-print(request)
-request = request.text
+request= requests.get(url, timeout=5).text
 
 #Passage dans BeautifulSoup
 soup = BeautifulSoup(request,'html.parser')
-cards = soup.find_all("div",class_ ='searchCardContainer')
-print(cards)
 
-def forming(var):
-    var_tab = []
-    for e in var : 
-        e = e.text
-        var_tab.append(e) 
+#Récupération des données
+cards = soup.find_all('div','Vehiculecard_Vehiculecard_cardBody')
+print (cards)
 
-    return var_tab
-
-
-soup = (soup.find_all('h3', {'class': 'Text_Text_text Vehiculecard_Vehiculecard_title Text_Text_subtitle2'}))
+for card in cards :
+    title = card.find('h3','Text_Text_subtitle2').get_text()
+    print(title)
+    motor = card.find('div', 'Text_Text_body2').get_text()
+    print(motor)
+    characteristics = card.find_all('div', 'Text_Text_text Vehiculecard_Vehiculecard_characteristicsItems Text_Text_body2')
+    for characteristic in characteristics : 
+        print(characteristic.get_text())
+    price = (card.find('span', 'Text_Text_text Vehiculecard_Vehiculecard_price Text_Text_subtitle2').get_text())
+    print(type(price))
+    print (price)
